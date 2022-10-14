9352ae8cca5f3f391e056c1bcf8241717518f25b
Anurag <anuragraju.480@gmail.com>
2022-10-14 09:16:38 +0530

    changes according to test cases

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -6,5 +6,18 @@ public class Main {
         String ans=obj.meth();
         System.out.println(ans);
     }
+    public static class A{
+        public static String meth(){
+            return "Invoking method from class A";
+        }
+    }
+    public static class B extends A{
+        public static String meth(){
+            A a=new A();
+            String ans=a.meth();
+            System.out.println(ans);
+            return "Method is overridden in Extendend class B";
+        }
+    }
 }
 
